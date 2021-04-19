ac735b89f97e9725dac9ebc6261d733bc6d0192b
Sorra2 <srtweed@stu.naperville203.org>
2021-04-19 09:01:02 -0500

    added better sprite and dash
    might need to play around with aerial movement and jump functions, also need to apply a few basic animations to character to implelent attack function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //public variables allow for easy testing, can be set to private once we find the sweet spot
     public float speed;
+    public float dashValue;
+    public float jumpValue;
     public GameObject character;
     public LayerMask groundLayer;
+    //character always starts out facing right, this is used for dash and attack
+    private static string facing = "right";
 
 
     void Update()
     {
+        //each of these are called every frame, can be changed if we want a delay
         Jump();
         Run();
+        Dash();
+
+        //this is just basic movement code
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         movement.Normalize();
         transform.position += movement * Time.deltaTime * speed;
+
+        //using the horizontal axis to determine which direction the character is facing, can also be useful for animations when we get there
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            facing = "left";
+        }
+        else if(Input.GetAxis("Horizontal") > 0)
+        {
+            facing = "right";
+        }
     }
 
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && Grounded())
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 100f), ForceMode2D.Impulse);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpValue), ForceMode2D.Impulse);
         }
         
     }
 
+    //Grounded() makes sure the character is on a block in the "ground" layer
     bool Grounded()
     {
         Vector2 position = transform.position;
@@ -51,6 +71,21 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.X))
         {
             speed -= 10;
+        }
+    }
+
+    void Dash()
+    {
+        //tempDash needed to prevent dashValue from changing
+        float tempDash = dashValue;
+        if (facing.Equals("left"))
+        {
+            tempDash *= -1;//multiply by -1 to make dash go to left
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(tempDash, 0f), ForceMode2D.Impulse);
         }
     }
 }
---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//i just stole this from super luigi lol
+public class Scroll : MonoBehaviour
+{
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+
+
+
+
+}
